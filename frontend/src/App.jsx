1d7332a2557d422ce51e6a9a95a1ef540743b0bf
CHINMAI HA <hachinmai25@gmail.com>
2026-02-25 15:46:46 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Status from './pages/Status'
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/status" element={<Status />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
