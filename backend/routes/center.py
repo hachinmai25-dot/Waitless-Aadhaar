6509929269bd9f93de079b5dec3b76ed5c0babea
CHINMAI HA <hachinmai25@gmail.com>
2026-03-05 21:39:02 +0530

    Update center.py

--- backend/routes/center.py
+++ backend/routes/center.py
@@ -1,1 +1,10 @@
+from fastapi import APIRouter
 
+router = APIRouter()
+
+@router.get("/centers")
+def get_centers():
+    return [
+        {"name": "Mysuru Aadhaar Center", "queue": 10},
+        {"name": "Bangalore Aadhaar Center", "queue": 25}
+    ]
