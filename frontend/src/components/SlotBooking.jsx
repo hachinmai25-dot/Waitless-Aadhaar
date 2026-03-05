cf8c835af69f29cfcef9ddf3a312a00382c32d65
CHINMAI HA <hachinmai25@gmail.com>
2026-03-05 21:52:02 +0530

    Update SlotBooking.jsx

--- frontend/src/components/SlotBooking.jsx
+++ frontend/src/components/SlotBooking.jsx
@@ -1,1 +1,22 @@
+function SlotBooking(){
 
+return(
+<div>
+
+<h3>Book Slot</h3>
+
+<select>
+<option>Center 1</option>
+<option>Center 2</option>
+</select>
+
+<input type="time"/>
+
+<button>Book</button>
+
+</div>
+)
+
+}
+
+export default SlotBooking
