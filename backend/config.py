b962935dfd16fee4377e936a34cf60d21e68e133
CHINMAI HA <hachinmai25@gmail.com>
2026-03-05 21:34:36 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,3 @@
-import os
-from dotenv import load_dotenv
+DATABASE_URL = "postgresql://user:password@localhost/aadhaar"
 
-load_dotenv()
-
-class Config:
-    MYSQL_HOST = os.getenv("DB_HOST", "localhost")
-    MYSQL_USER = os.getenv("DB_USER", "root")
-    MYSQL_PASSWORD = os.getenv("DB_PASSWORD", "")
-    MYSQL_DB = os.getenv("DB_NAME", "waitless_aadhaar")
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    GOOGLE_MAPS_API = os.getenv("GOOGLE_MAPS_API", "")
+MAX_QUEUE_PER_CENTER = 50
