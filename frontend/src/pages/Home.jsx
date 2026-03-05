d873ab2495552bf7e596401103fb70778488fd2a
CHINMAI HA <hachinmai25@gmail.com>
2026-03-05 21:53:28 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,20 @@
+import RegistrationForm from "../components/RegistrationForm"
+import SlotBooking from "../components/SlotBooking"
 
+function Home(){
+
+return(
+<div>
+
+<h1>Skip Aadhaar Waiting Lines</h1>
+
+<RegistrationForm/>
+
+<SlotBooking/>
+
+</div>
+)
+
+}
+
+export default Home
